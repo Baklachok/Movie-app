81f4f4a3e789bc2d64cb2f11e3cff299f4c72142
Даниил Коробко <korobko_01@inbox.ru>
2023-12-17 21:52:29 +0300

    refactored django models

--- films/models.py
+++ films/models.py
@@ -7,8 +7,17 @@ class User(AbstractUser):
     pass
 
 class Film(models.Model):
-    name = models.CharField(max_length=128, unique=True)
-    users = models.ManyToManyField(User, related_name='films')
+    name = models.CharField(max_length=128)
+    users = models.ManyToManyField(User, related_name='films', through='UserFilms')
 
     class Meta:
         ordering = [Lower('name')]
+
+
+class UserFilms(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE)
+    order = models.PositiveSmallIntegerField()
+
+    class Meta:
+        ordering = ['order']
